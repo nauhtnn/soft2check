a0bbf7f962660c39a6b86b51dd088ca146152b50
Phung1997 <kimphung1071997@gmail.com>
2018-03-30 15:11:06 +0700

    +phung sua vai cho

--- Client99/PhungTest.cs
+++ Client99/PhungTest.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Client99
 {
     class PhungTest
     {
         public PhungTest() { }
+       
 
         public static string TestAndReport()
         {
-            string report = "Chưa kiểm tra files, Unikey.";
-            ////////////////start test
+            string report = "Chưa kiểm tra File.";
+            DirectoryInfo dirStanford = new DirectoryInfo("D:\\Stanford");
+            DirectoryInfo[] dirSub = dirStanford.GetDirectories();
+            foreach (DirectoryInfo folder in dirSub)
+            {
+                report = "Cac file ton tai:";
+                
+            }
 
-            //test here
-
-            ////////////////end test
             return report;
         }
+        
+
+
     }
+
+
+
+
+    //test here
+
+    ////////////////end test
 }
+                    
