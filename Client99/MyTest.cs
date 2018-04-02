4ed907018b2843f68a38da5e87ff1d240570b4c9
KieuMy <kieumyk40@gmail.com>
2018-04-02 13:57:14 +0700

    My kiem tra thu muc office ton tai

--- Client99/MyTest.cs
+++ Client99/MyTest.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 namespace Client99
 {
     class MyTest
@@ -16,7 +21,23 @@ namespace Client99
             ////////////////start test
 
             //test here
+            string filePath = @"C:/Program Files/Microsoft Office/Office14/EXCEL.exe";
+            if (System.IO.File.Exists(filePath))
+               report +="Tồn tại  \"Excel.exe\".";
+            else
+               report+="Không tồn tại \"Excel.exe\".";
 
+            string filePath2 = @"C:/Program Files/Microsoft Office/Office14/POWERPNT.exe";
+            if (System.IO.File.Exists(filePath2))
+                report += "Tồn tại  \"POWERPOINT.exe\".";
+            else
+                report += "Không tồn tại \"POWERPOINT.exe\".";
+
+            string filePath3 = @"C:/Program Files/Microsoft Office/Office14/WINWORD.exe";
+            if (System.IO.File.Exists(filePath3))
+                report += "Tồn tại  \"WINWORD.exe\".";
+            else
+                report += "Không tồn tại \"WINWORD.exe\".";
             ////////////////end test
             return report;
         }
