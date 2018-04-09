0d29030868b8f698f4c2496a29ad6ac702cdac28
KieuMy <kieumyk40@gmail.com>
2018-04-09 16:23:33 +0700

    My working on interop.word

--- Client99/MyTest.cs
+++ Client99/MyTest.cs
@@ -12,6 +12,8 @@ using System.Drawing;
 using Word = Microsoft.Office.Interop.Word;
 using Powerpoint = Microsoft.Office.Interop.PowerPoint;
 using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.PowerPoint;
+
 namespace Client99
 {
     class MyTest
@@ -28,7 +30,7 @@ namespace Client99
                 get; set;
             }
         }
-
+                  
 
         public static string TestAndReport()
         {
@@ -58,68 +60,87 @@ namespace Client99
 
             // goi excel
 
-            /*  if (System.IO.File.Exists(excel_file))//xoa file kiem tra cu
-                  System.IO.File.Delete(excel_file);
+            /*   if (System.IO.File.Exists(excel_file))//xoa file kiem tra cu
+                   System.IO.File.Delete(excel_file);
 
-               var bankAccounts = new List<Account>
-               {
-                   new Account
-                           {
-                           ID = 01,
-                           Balance = 8
-                           },
-                   new Account
-                           {
-                           ID = 02,
-                           Balance = 5
+                var bankAccounts = new List<Account>
+                {
+                    new Account
+                            {
+                            ID = 01,
+                            Balance = 8
+                            },
+                    new Account
+                            {
+                            ID = 02,
+                            Balance = 5
 
-                           }
-                };
+                            }
+                 };
 
-               var excelApp = new Excel.Application();
-                   excelApp.Visible = true;
-              Microsoft.Office.Interop.Excel.Workbook wbook = excelApp.Workbooks.Add();
-                   Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+                var excelApp = new Excel.Application();
+                    excelApp.Visible = true;
+               Microsoft.Office.Interop.Excel.Workbook wbook = excelApp.Workbooks.Add();
+                    Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
 
-                   workSheet.Cells[1, "A"] = "MASV";
-                   workSheet.Cells[1, "B"] = "DiemThi";
-               var row = 1;
-               foreach (var acct in  bankAccounts )
-               {
-                   row++;
-                   workSheet.Cells[row, "A"] = acct.ID;
-                   workSheet.Cells[row, "B"] = acct.Balance;
-               }
-                   workSheet.Columns[1].AutoFit();
-                   workSheet.Columns[2].AutoFit();
-                   ((Excel.Range)workSheet.Columns[1]).AutoFit();
-                   ((Excel.Range)workSheet.Columns[2]).AutoFit();
+                    workSheet.Cells[1, "A"] = "MASV";
+                    workSheet.Cells[1, "B"] = "DiemThi";
+                var row = 1;
+                foreach (var acct in  bankAccounts )
+                {
+                    row++;
+                    workSheet.Cells[row, "A"] = acct.ID;
+                    workSheet.Cells[row, "B"] = acct.Balance;
+                }
+                    workSheet.Columns[1].AutoFit();
+                    workSheet.Columns[2].AutoFit();
+                    ((Excel.Range)workSheet.Columns[1]).AutoFit();
+                    ((Excel.Range)workSheet.Columns[2]).AutoFit();
 
-              //excelApp.Visible = false;
-              //Directory.SetCurrentDirectory("d:/");
-              //wbook.SaveAs("kiemtra.xlsx", Excel.XlFileFormat.xlWorkbookDefault);
-              wbook.SaveAs(excel_file, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
-              false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
-              Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
-              wbook.Close();
-              excelApp.Workbooks.Close();
-              excelApp.Quit();
-              report += "\nĐÃ KIỂM TRA EXCEL";*/
-            
-            
+               //excelApp.Visible = false;
+               //Directory.SetCurrentDirectory("d:/");
+               //wbook.SaveAs("kiemtra.xlsx", Excel.XlFileFormat.xlWorkbookDefault);
+               wbook.SaveAs(excel_file, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+               false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+               Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+               wbook.Close();
+               excelApp.Workbooks.Close();
+               excelApp.Quit();
+               report += "\nĐÃ KIỂM TRA EXCEL";*/
+
+
             // kiem tra word.
 
-            object oMissing = System.Reflection.Missing.Value;
-            object oEndOfDoc = "\\endofdoc";
+            //object omissing = system.reflection.missing.value;
+            //object oendofdoc = "\\endofdoc";
+            //start word and create a new document.
+            //word.application oword;
+            //word.document odoc;
+            //oword = new word.application();
+            //oword.visible = true;
+            //odoc = oword.documents.add(ref omissing, ref omissing, ref omissing, ref omissing);*/
 
-            //Start Word and create a new document.
+            Word.Application wordApp = new Word.Application();
+            wordApp.Visible = true;
+             //wordapp.documents.add();
+             //wordapp.selection.pastespecial(link: true, displayasicon: true);
 
-            Word.Application oWord;
-            Word.Document oDoc;
-            oWord = new Word.Application();
-            oWord.Visible = true;
-            oDoc = oWord.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);
-       
+            //kiem tra ppt
+            //Application pptApplication = new Application();
+
+            //Microsoft.Office.Interop.PowerPoint.Slides slides;
+            //Microsoft.Office.Interop.PowerPoint._Slide slide;
+            //Microsoft.Office.Interop.PowerPoint.TextRange objText;
+
+            //// Create the Presentation File
+            //Presentation pptPresentation = pptApplication.Presentations.Add(MsoTriState.msoTrue);
+
+            //Microsoft.Office.Interop.PowerPoint.CustomLayout customLayout = pptPresentation.SlideMaster.CustomLayouts[Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutText];
+
+            //// Create new Slide
+            //slides = pptPresentation.Slides;
+            //slide = slides.AddSlide(1, customLayout);
+
             ////////////////end test
             return report;
         }
