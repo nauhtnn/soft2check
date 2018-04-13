a91b35b684d20d2bddd998e63d1be22706bf9ed2
KieuMy <kieumyk40@gmail.com>
2018-04-13 10:49:07 +0700

    my mo va gia lap thao tac tren word

--- Client99/MyTest.cs
+++ Client99/MyTest.cs
@@ -12,6 +12,7 @@ using System.Drawing;
 using Excel = Microsoft.Office.Interop.Excel;
 using Word = Microsoft.Office.Interop.Word;
 using PowerPoin = Microsoft.Office.Interop.PowerPoint;
+using GemBox.Document;
 /*using System.Windows;
 using Spire.Doc;
 using Spire.Doc.Documents;*/
@@ -88,8 +89,8 @@ namespace Client99
             //    bankAccounts.Add(acc);
             //}
 
-            var excelApp = new Excel.Application();
-            excelApp.Visible = true;
+            /*  var excelApp = new Excel.Application();
+              excelApp.Visible = true;*/
             // Microsoft.Office.Interop.Excel.Workbook wbook = excelApp.Workbooks.Add();
             //      Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
 
@@ -134,20 +135,20 @@ namespace Client99
 
             //}
 
-            Excel.Workbook ex_sample = excelApp.Workbooks.Add(@"D:\users\repos\soft2check\dat\sample.xlsx");
-            Excel.Worksheet ws_samp = ex_sample.ActiveSheet;
-            Excel.Workbook ex_m = excelApp.Workbooks.Add(excel_file_ext);
-            Excel.Worksheet ws_m = ex_m.ActiveSheet;
+            /* Excel.Workbook ex_sample = excelApp.Workbooks.Add(@"D:\users\repos\soft2check\dat\sample.xlsx");
+             Excel.Worksheet ws_samp = ex_sample.ActiveSheet;
+             Excel.Workbook ex_m = excelApp.Workbooks.Add(excel_file_ext);
+             Excel.Worksheet ws_m = ex_m.ActiveSheet;
 
-            int i = 0;
-            foreach(Excel.Range c in ws_samp.Columns)
-            {
-                if (3 < ++i)
-                    break;
-                c.Copy();
-                ws_m.Columns["A"].PasteSpecial();
-            }
-
+             int i = 0;
+             foreach(Excel.Range c in ws_samp.Columns)
+             {
+                 if (3 < ++i)
+                     break;
+                 c.Copy();
+                 ws_m.Columns["A"].PasteSpecial();
+             }
+             */
             //ws.Select(true);
             //ws.Cells.Select();
 
@@ -184,20 +185,17 @@ namespace Client99
             //excelApp.DisplayAlerts = true;
 
             //wbook.Close();
-              excelApp.Workbooks.Close();
+            /*  excelApp.Workbooks.Close();
               excelApp.Quit();
-              report += "\nĐÃ KIỂM TRA EXCEL";
-            ////////////////end test
+              report += "\nĐÃ KIỂM TRA EXCEL";*/
+
 
             //kt wprd
-            var wordApp = new Word.Application();
+            /*var wordApp = new Word.Application();
             wordApp.Visible = true;
-            wordApp.Documents.Add();
-            wordApp.Selection.PasteSpecial(Link: true, DisplayAsIcon: true);
+            wordApp.Documents.Add();*/
+            // wordApp.Selection.PasteSpecial(Link: true, DisplayAsIcon: true);
 
-
-            // kt powerpoint
-            
             /* Document doc = new Document();
              //Add Section
              Section section = doc.AddSection();
@@ -209,6 +207,142 @@ namespace Client99
                  + "Word document) without installing Microsoft Office and any other third-party tools on system.");*/
 
 
+            object oMissing = System.Reflection.Missing.Value;
+            object oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+            oWord.Visible = true;
+            oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
+            ref oMissing, ref oMissing);
+
+            //Insert a paragraph at the beginning of the document.
+            Word.Paragraph oPara1;
+            oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
+            oPara1.Range.Text = "hello word ";
+            oPara1.Range.Font.Bold = 1;
+            oPara1.Format.SpaceAfter = 24;    //24 pt spacing after paragraph.
+            oPara1.Range.InsertParagraphAfter();
+            //Insert a paragraph at the end of the document.
+            Word.Paragraph oPara2;
+            object oRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oPara2 = oDoc.Content.Paragraphs.Add(ref oRng);
+            oPara2.Range.Text = "troi oi xong roi";
+            oPara2.Format.SpaceAfter = 6;
+            oPara2.Range.InsertParagraphAfter();
+            //Insert another paragraph.
+            Word.Paragraph oPara3;
+            oRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oPara3 = oDoc.Content.Paragraphs.Add(ref oRng);
+            oPara3.Range.Text = "This is a sentence of normal text. Now here is a table:";
+            oPara3.Range.Font.Bold = 0;
+            oPara3.Format.SpaceAfter = 24;
+            oPara3.Range.InsertParagraphAfter();
+
+           
+
+            //Insert a 3 x 5 table, fill it with data, and make the first row
+            //bold and italic.
+            Word.Table oTable;
+            Word.Range wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oTable = oDoc.Tables.Add(wrdRng, 3, 5, ref oMissing, ref oMissing);
+            oTable.Range.ParagraphFormat.SpaceAfter = 6;
+            int r, c;
+            string strText;
+            for (r = 1; r <= 3; r++)
+                for (c = 1; c <= 5; c++)
+                {
+                    strText = "r" + r + "c" + c;
+                    oTable.Cell(r, c).Range.Text = strText;
+                }
+            oTable.Rows[1].Range.Font.Bold = 1;
+            oTable.Rows[1].Range.Font.Italic = 1;
+
+            //Add some text after the table.
+            Word.Paragraph oPara4;
+            oRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oPara4 = oDoc.Content.Paragraphs.Add(ref oRng);
+            oPara4.Range.InsertParagraphBefore();
+            oPara4.Range.Text = "And here's another table:";
+            oPara4.Format.SpaceAfter = 24;
+            oPara4.Range.InsertParagraphAfter();
+
+            //Insert a 5 x 2 table, fill it with data, and change the column widths.
+            wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oTable = oDoc.Tables.Add(wrdRng, 5, 2, ref oMissing, ref oMissing);
+            oTable.Range.ParagraphFormat.SpaceAfter = 6;
+            for (r = 1; r <= 5; r++)
+                for (c = 1; c <= 2; c++)
+                {
+                    strText = "r" + r + "c" + c;
+                    oTable.Cell(r, c).Range.Text = strText;
+                }
+            oTable.Columns[1].Width = oWord.InchesToPoints(2); //Change width of columns 1 & 2
+            oTable.Columns[2].Width = oWord.InchesToPoints(3);
+
+            //Keep inserting text. When you get to 7 inches from top of the
+            //document, insert a hard page break.
+            object oPos;
+            double dPos = oWord.InchesToPoints(7);
+            oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range.InsertParagraphAfter();
+            do
+            {
+                wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+                wrdRng.ParagraphFormat.SpaceAfter = 6;
+                wrdRng.InsertAfter("A line of text");
+                wrdRng.InsertParagraphAfter();
+                oPos = wrdRng.get_Information
+                                       (Word.WdInformation.wdVerticalPositionRelativeToPage);
+            }
+            while (dPos >= Convert.ToDouble(oPos));
+            object oCollapseEnd = Word.WdCollapseDirection.wdCollapseEnd;
+            object oPageBreak = Word.WdBreakType.wdPageBreak;
+            wrdRng.Collapse(ref oCollapseEnd);
+            wrdRng.InsertBreak(ref oPageBreak);
+            wrdRng.Collapse(ref oCollapseEnd);
+            wrdRng.InsertAfter("We're now on page 2. Here's my chart:");
+            wrdRng.InsertParagraphAfter();
+
+            //Insert a chart.
+            Word.InlineShape oShape;
+            object oClassType = "MSGraph.Chart.8";
+            wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            oShape = wrdRng.InlineShapes.AddOLEObject(ref oClassType, ref oMissing,
+            ref oMissing, ref oMissing, ref oMissing,
+            ref oMissing, ref oMissing, ref oMissing);
+
+            //Demonstrate use of late bound oChart and oChartApp objects to
+            //manipulate the chart object with MSGraph.
+            object oChart;
+            object oChartApp;
+            oChart = oShape.OLEFormat.Object;
+            oChartApp = oChart.GetType().InvokeMember("Application",
+            BindingFlags.GetProperty, null, oChart, null);
+
+            //Change the chart type to Line.
+            object[] Parameters = new Object[1];
+            Parameters[0] = 4; //xlLine = 4
+            oChart.GetType().InvokeMember("ChartType", BindingFlags.SetProperty,
+            null, oChart, Parameters);
+
+            //Update the chart image and quit MSGraph.
+            oChartApp.GetType().InvokeMember("Update",
+            BindingFlags.InvokeMethod, null, oChartApp, null);
+            oChartApp.GetType().InvokeMember("Quit",
+            BindingFlags.InvokeMethod, null, oChartApp, null);
+            //... If desired, you can proceed from here using the Microsoft Graph 
+            //Object model on the oChart and oChartApp objects to make additional
+            //changes to the chart.
+
+            //Set the width of the chart.
+            oShape.Width = oWord.InchesToPoints(6.25f);
+            oShape.Height = oWord.InchesToPoints(3.57f);
+
+            //Add text after the chart.
+            wrdRng = oDoc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+            wrdRng.InsertParagraphAfter();
+            wrdRng.InsertAfter("THE END.");
+            ////////////////end test
             return report;
         }
     }
